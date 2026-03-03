702664afa616dfa12a6f50959089e410b1028432
Serena <111618928+skrueg1@users.noreply.github.com>
2026-03-03 16:57:31 +0100

    Resized images, added font, restructured folders

--- diceapp.py
+++ diceapp.py
@@ -9,13 +9,12 @@ def createWidget(parent, widgetType, **options):
     return widgetType(parent, **options)
 
 def createDiceButton(parent, text, image):
-    path = "images/" + text + ".png"
     return createWidget(
         parent, tk.Button,
         bd = 0, cursor = 'hand2',
-        bg = container.cget("bg"),
+        bg = root.cget("bg"),
         fg = "black", font = ("Arial", 12, "bold"),
-        activebackground = container.cget("bg"),
+        activebackground = root.cget("bg"),
         activeforeground = None,
         relief = "flat",
         command=lambda d=die: selectDice(d),
@@ -40,37 +39,27 @@ def roll():
 # --- CREATE WINDOW ---
 root = createWidget(None, tk.Tk)
 root.title("Dice Roller")
-wWidth, wHeight = 1100, 800
+wWidth, wHeight = 1200, 1000
+root.config(bg = '#1E0F11')
 ## Center the pop-up
 centerX = (root.winfo_screenwidth() // 2) - (wWidth // 2)
 centerY = (root.winfo_screenheight() // 2) - (wHeight // 2)
 root.geometry(f"{wWidth}x{wHeight}+{centerX}+{centerY}")
 
 # --- UI ---
-## Content container 
-container = createWidget(
-    root, tk.Frame,
-    height = 600, width = 1000,
-    bg='lightblue', bd=3,
-    highlightcolor='red',
-    highlightbackground='black',
-    highlightthickness=2,
+## Load Title Photo
+titlePhoto = PhotoImage(file = "assets/images/upperUI.png")
+titleImage = createWidget(
+    root, tk.Label,
+    image = titlePhoto,
+    bg = root.cget("bg")
 )
-container.pack()
-container.pack_propagate(False)
-
-## Displayed app title
-appTitle = createWidget(
-    container, tk.Label,
-    text='TTRPG Digital Dice Roller',
-    font=("Arial", 24, ), 
-    bd = 0, bg = None
-)
-appTitle.pack(padx=50, pady=20)
+titleImage.photo = titlePhoto
+titleImage.pack(pady = (50, 0))
 
 ## Create dice menu
 diceDisplay = createWidget(
-    container, tk.Frame,
+    root, tk.Frame,
     height = 160, width = 1000
 )
 diceDisplay.pack()
@@ -78,15 +67,16 @@ diceTypes = ["d4", "d6", "d8", "d10", "d12", "d20"]
 diceButtons = {}
 selected = None # no dice is selected when window opens
 for die in diceTypes:
-    photo = PhotoImage(file = "images/" + die + ".png")
+    photo = PhotoImage(file = "assets/images/" + die + ".png")
     diceButton = createDiceButton(diceDisplay, die, photo)
     diceButton.pack(side = LEFT)
     diceButton.pack_propagate(False)
     diceButtons[die] = (diceButton,photo)
+diceDisplay.pack(pady = 10)
 
 ## Display result
 resultLabel = createWidget(
-    container, tk.Label,
+    root, tk.Label,
     text='Result: ',
     font='50', bg='lightblue',
     bd=3, cursor='hand2',
@@ -94,7 +84,17 @@ resultLabel = createWidget(
     highlightbackground='black',
     highlightthickness=2
 )
-resultLabel.pack(pady=5)
+resultLabel.pack(pady = (20,0))
+
+## Load Background photo
+backgroundPhoto = PhotoImage(file = "assets/images/lowerUI.png")
+backgroundImage = createWidget(
+    root, tk.Label,
+    image = backgroundPhoto,
+    bg = root.cget("bg")
+)
+backgroundImage.photo = backgroundPhoto
+backgroundImage.pack(pady = (50, 0))
 
 # --- MAIN ---
 root.mainloop()
